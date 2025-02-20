5bbc78346223442d7dc701eec738da45c27bf49a
mubashirbappi <mubashirbappi@gmail.com>
2025-02-20 23:32:29 +0600

    firebace google login function implement

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,11 +1,20 @@
 import { createContext } from "react";
-
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import auth from "../services/Firebace.init";
 // eslint-disable-next-line react-refresh/only-export-components
 export const AuthProvider = createContext();
 // eslint-disable-next-line react/prop-types
 const AuthContext = ({children}) => {
+
+    const provider = new GoogleAuthProvider();
+
+  const googlelogin = ()=>{
+    return signInWithPopup(auth, provider)
+  }
+
     const authInfo = {
-        name:'bappi'
+        name:'bappi',
+        googlelogin 
     }
     return (
         <AuthProvider.Provider value={authInfo}>
