75bc697bc394fd7678b9b219b24d0d2b90611c91
mubashirbappi <mubashirbappi@gmail.com>
2025-02-21 11:34:01 +0600

    delete function added

--- src/Components/TaskCard.jsx
+++ src/Components/TaskCard.jsx
@@ -1,21 +1,31 @@
 /* eslint-disable react/prop-types */
 
+import axios from 'axios';
+import moment from 'moment';
+import { MdDelete, MdEdit } from 'react-icons/md';
+
 
 const TaskCard = ({ task, index }) => {
-    const {title,description,timestamp}=task
+    const {title,description,timestamp,_id}=task
+    const handleDelete = ()=>{
+        axios.delete(`http://localhost:5000/tasks/${_id}`)
+    }
+    
   return (
    
         <div
         
-          className="p-3 bg-white shadow rounded mt-2"
+          className="p-3 w-full md:w-72 bg-white shadow rounded mt-2 flex justify-between"
         >
-          <h3 className="font-bold">{title}</h3>
-          <h5>{timestamp}</h5>
-          <p className="text-sm">{description}</p>
-
           <div>
-            <button>Edit</button>
-            <button>Delete</button>
+          <h3 className="font-bold">{title}</h3>
+          <h5>{moment(timestamp).add(10, 'days').calendar()}</h5>
+          <p className="text-sm">{description}</p>
+          </div>
+
+          <div className='flex flex-col gap-3'>
+            <button className='cursor-pointer text-blue-500'><MdEdit/></button>
+            <button onClick={handleDelete} className='cursor-pointer text-red-500'><MdDelete /></button>
           </div>
         </div>
      
